a9368282feabfdc7558e0f4579bf948ea87211ac
alex_kasper <alex_kasper@gmx.de>
2009-07-01 08:17:26 +0000

    "Fixed" event declarations in OViSEMessenger

--- OViSE/OViSESceneHandling.h
+++ OViSE/OViSESceneHandling.h
@@ -36,10 +36,6 @@
 #include <wx/bitmap.h>
 #include <wx/filename.h>
 
-// Declare event for scene changes
-//DECLARE_EVENT_TYPE(wxEVT_OGRE_SCENE_CHANGED, -1)
-//DEFINE_EVENT_TYPE(wxEVT_OGRE_SCENE_CHANGED)
-
 /// Map containing all scene managers.
 typedef std::map<std::string, Ogre::SceneManager*> OViSEScnMgrMap;
 /// Map containing all ray scene querys for object selection.
