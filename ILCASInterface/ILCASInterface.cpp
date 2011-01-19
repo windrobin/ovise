b65cb1e23959f0b6ec57facfac0f81f3043ab8bc
alexander limubei kasper <alexander.limubei.kasper@googlemail.com>
2011-01-19 10:32:06 +0100

    * started switch to CMake * added ILCASInterface

---
+++ ILCASInterface/ILCASInterface.cpp
@@ -0,0 +1,52 @@
+#include "ILCASInterface.h"
+
+#include <boost/lexical_cast.hpp>
+
+
+CILCASInterface::CILCASInterface( boost::asio::io_service& IOService, EntityPool& EntPool )
+	: CNetworkInterface( IOService, EntPool ),
+	mEndpoint( boost::asio::ip::tcp::v4(), 2121 ),
+	mAcceptor( mIOService ),
+	mSocket( mIOService ),
+	mMessageHandler( &EntPool )
+{
+	mAcceptor.listen();
+	mAcceptor.async_accept( mSocket,
+		boost::bind( &CILCASInterface::AcceptHandler, this, _1 ) );
+}
+
+
+CILCASInterface::~CILCASInterface(void)
+{
+}
+
+void CILCASInterface::WriteHandler( const boost::system::error_code&, std::size_t )
+{
+}
+
+void CILCASInterface::ReadHandler( const boost::system::error_code& Error, std::size_t BytesTransferred )
+{
+	if( Error )
+		return;
+
+	std::cout << std::string( mBuffer.data(), BytesTransferred ) << std::endl;
+
+	const char* Data = boost::asio::buffer_cast<const char*>( mStreamBuffer.data() );
+	int Id = mMessageHandler.HandleMessage( Data );
+	mStreamBuffer.consume( BytesTransferred );
+
+	boost::asio::write( mSocket, boost::asio::buffer( boost::lexical_cast<std::string>( Id ) ) );
+
+	boost::asio::async_read_until( mSocket, mStreamBuffer, "</Entity>", 
+		boost::bind( &CILCASInterface::ReadHandler, this, _1, _2 ) );
+}
+
+void CILCASInterface::AcceptHandler( const boost::system::error_code& Error )
+{
+	if( Error )
+		return;
+
+	boost::asio::write( mSocket, boost::asio::buffer( "ready" ) );
+	boost::asio::async_read_until( mSocket, mStreamBuffer, "</Entity>", 
+		boost::bind( &CILCASInterface::ReadHandler, this, _1, _2 ) );
+}
---
+++ ILCASInterface/ILCASInterface.h
@@ -0,0 +1,29 @@
+#ifndef OVISE_ILCAS_INTERFACE_H
+#define OVISE_ILCAS_INTERFACE_H
+
+#include "../OViSEAux/NetworkInterface.h"
+#include "SocketMessage.h"
+
+class CILCASInterface :
+	public CNetworkInterface
+{
+public:
+	CILCASInterface( boost::asio::io_service& IOService, EntityPool& EntPool );
+	~CILCASInterface(void);
+
+	void WriteHandler( const boost::system::error_code& , std::size_t );
+	void AcceptHandler( const boost::system::error_code& );
+	void ReadHandler( const boost::system::error_code&, std::size_t );
+
+private:
+	boost::asio::ip::tcp::endpoint	mEndpoint;
+	boost::asio::ip::tcp::acceptor	mAcceptor;
+	boost::asio::ip::tcp::socket	mSocket;
+
+	boost::array<char, 4096>		mBuffer;
+	boost::asio::streambuf			mStreamBuffer;
+
+	SocketMessage					mMessageHandler;
+};
+
+#endif // OVISE_ILCAS_INTERFACE_H
