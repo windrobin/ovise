f27f351a90ba9da6ac959f172e8446f57645e8ef
Alexander Kasper <alexander.kasper@kit.edu>
2011-04-05 17:34:31 +0200

    * removed old sconscript file

--- custom.py
+++
@@ -1,9 +0,0 @@
-# -*- python -*-
-# This is a sample configuration file, alter values to match your system
-
-# Path to wxWidgets installation/source dir
-WXCONFIG = '/usr/local/bin/'
-# Variable defining if Ogre flags should be retrieved via pkg-config
-OGRE_PKG = True
-# Path to Ogre installation (only used when OGRE_PKG=False)
-OGRE_PATH = '/usr/local/lib'
