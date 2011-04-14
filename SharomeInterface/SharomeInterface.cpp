528c365e4a21efdd45b8f9d9adb9e4421bf39c67
Alexander Kasper <alexander.kasper@kit.edu>
2011-04-14 11:14:38 +0200

    * changed SharomeInterface to use provided CAsyncClient * adjusted linkage * adjusted handling of object changes

--- SharomeInterface/SharomeInterface.cpp
+++ SharomeInterface/SharomeInterface.cpp
@@ -42,77 +42,6 @@ namespace
 			}
 		}
 	}
-
-	void SyncAttribs( Entity* OldEnt, const OOWM::Mem::CObj& Obj )
-	{
-		// check if we need to remove any attributes
-		const Entity::AttributeType& Atts = OldEnt->GetAttributes();
-		std::vector<std::string> ToRemove;
-		for( Entity::AttributeType::const_iterator i = Atts.begin();
-			i != Atts.end(); i++ )
-		{
-			OOWM::Mem::CAttribute Attrib;
-			OOWM::SResult Result;
-			Obj.getFirstByName( Attrib, i->first, &Result );
-			if( Result.m_nCode != R_OK )
-				ToRemove.push_back( i->first );
-		}
-		BOOST_FOREACH( std::string s, ToRemove )
-		{
-			OldEnt->RemoveAttribute( s );
-		}
-		// go through all attributes of updated object and add/update as necessary
-		std::set<std::string> Ids;
-		OOWM::SResult Result;
-		Obj.getIdsByName( Ids, "*", &Result );
-		if( Result.m_nCode != R_OK ) return;
-
-		for( std::set<std::string>::iterator i = Ids.begin(); i != Ids.end(); i++ )
-		{
-			OOWM::Mem::CAttribute CurrentAttribute;
-			Obj.getById( CurrentAttribute, *i, &Result );
-
-			if( CurrentAttribute.getName() == "Location" )
-			{
-				OOWM::Mem::CLocation L;
-				if( L.FromAttribute( CurrentAttribute ) )
-				{
-					OldEnt->SetAttribute( "Position",
-						vec3( float(L.m_Position.m_Value[0]),
-							float(L.m_Position.m_Value[1]),
-							float(L.m_Position.m_Value[2] ) ) );
-					OldEnt->SetAttribute( "Orientation",
-						quat( float(L.m_Orientation.m_Value[0]),
-						float(L.m_Orientation.m_Value[1]),
-						float(L.m_Orientation.m_Value[2]),
-						float(L.m_Orientation.m_Value[3] ) ) );
-					OldEnt->SetAttribute( "Scale",
-						vec3( float(L.m_Scale.m_Value[0]),
-						float(L.m_Scale.m_Value[1]),
-						float(L.m_Scale.m_Value[2] ) ) );
-				}
-			}
-			else
-			{
-				if( Result.m_nCode != R_OK ) continue;
-				std::set<std::string> ValueIds;
-				CurrentAttribute.getIdsByName( ValueIds, "*", &Result );
-				if( Result.m_nCode != R_OK ) continue;
-				for( std::set<std::string>::iterator i = ValueIds.begin(); i != ValueIds.end(); i++ )
-				{
-					OOWM::Mem::CValue Val;
-					CurrentAttribute.getById( Val, *i, &Result );
-					if( Result.m_nCode != R_OK ) continue;
-					OOWM::Mem::ValueType Value;
-					OOWM::Mem::DeviationType Dev;
-					OOWM::Mem::WeightType Weights;
-					Val.get( Value, Dev, Weights, &Result );
-					if( Result.m_nCode != R_OK ) continue;
-					OldEnt->SetAttribute( Val.getName(), Value );
-				}
-			}
-		}
-	}
 }
 
 
@@ -129,22 +58,22 @@ bool SharomeInterface::Start()
 	std::cout << "SharomeInterface init." << std::endl;
 
 	// FIXME: to be set by dialog
-	std::string Host = "i61pst1";
+	std::string Host = "localhost";
 	std::string Service = "12345";
 
-	boost::asio::ip::tcp::resolver           Resolver( mIOService );
-	boost::asio::ip::tcp::resolver::query    Query( Host, Service );
-	boost::asio::ip::tcp::resolver::iterator EndpointIterator =
-	        Resolver.resolve( Query );
-	boost::asio::ip::tcp::endpoint Endpoint = *EndpointIterator;
-
-	mConnection.reset( new CConnection( mIOService ) );
-
-	mConnection->Socket().async_connect( Endpoint,
-		boost::bind( &SharomeInterface::HandleConnect, this,
-			boost::asio::placeholders::error, ++EndpointIterator ) );
-
-	mRunning = true;
+	LegacyClient.reset( new CAsyncClient( Host, Service, true ) );
+	LegacyClient->ObjectCreatedSignal.connect(
+		boost::bind( &SharomeInterface::HandleObjectCreated, this, _1 ) );
+	LegacyClient->ObjectChangedSignal.connect(
+		boost::bind( &SharomeInterface::HandleObjectChanged, this, _1 ) );
+	LegacyClient->ObjectDeletedSignal.connect(
+		boost::bind( &SharomeInterface::HandleObjectDeleted, this, _1 ) );
+	LegacyClient->SceneChangedSignal.connect(
+		boost::bind( &SharomeInterface::HandleSceneChanged, this, _1 ) );
+	LegacyClient->SceneDeletedSignal.connect(
+		boost::bind( &SharomeInterface::HandleSceneDeleted, this ) );
+	LegacyClient->ErrorSignal.connect(
+		boost::bind( &SharomeInterface::HandleError, this, _1 ) );
 
 	return true;
 }
@@ -152,133 +81,15 @@ bool SharomeInterface::Start()
 bool SharomeInterface::Stop()
 {
 	std::cout << "SharomeInterface uninit." << std::endl;
-	mConnection->Socket().close();
-
-	mRunning = false;
+	LegacyClient.reset();
 
 	return true;
 }
 
 void SharomeInterface::Poll()
 {
-	mIOService.poll_one();
-	mIOService.reset();
-}
-
-void SharomeInterface::HandleConnect(
-        const boost::system::error_code& Error,
-        boost::asio::ip::tcp::resolver::iterator
-        EndpointIterator )
-{
-	if(!Error)
-	{
-		boost::asio::socket_base::keep_alive option( true );
-		mConnection->Socket().set_option( option );
-
-		// tell server we want the whole scene
-		boost::shared_ptr<Comm::Message> msg( new Comm::Message );
-		msg->m_Code = Comm::MSG_GET;
-		msg->m_Type = Comm::TYPE_CSCENE;
-
-		mConnection->AsyncWrite( msg,
-			boost::bind( &SharomeInterface::HandleWrite, this,
-				boost::asio::placeholders::error ) );
-	}
-	else if( EndpointIterator != boost::asio::ip::tcp::resolver::iterator())
-	{
-		mConnection->Socket().close();
-		boost::asio::ip::tcp::endpoint Endpoint = *EndpointIterator;
-		mConnection->Socket().async_connect( Endpoint,
-			boost::bind( &SharomeInterface::HandleConnect, this,
-				boost::asio::placeholders::error,
-				++EndpointIterator ) );
-	}
-	else
-	{
-		std::cerr << Error.message() << std::endl;
-	}
-}
-
-void SharomeInterface::HandleWrite( const boost::system::error_code& Error )
-{
-	if(!Error)
-	{
-		boost::shared_ptr<Comm::Message> Reply;
-		Reply.reset( new Comm::Message );
-		mConnection->AsyncRead( Reply,
-			boost::bind( &SharomeInterface::HandleRead, this,
-				boost::asio::placeholders::error, Reply ) );
-	}
-	else
-	{
-		std::cerr << Error.message() << std::endl;
-	}
-}
-
-void SharomeInterface::HandleRead( const boost::system::error_code& Error,
-                                   boost::shared_ptr<Comm::Message> Msg )
-{
-	if(Error)
-	{
-		std::cerr << Error.message() << std::endl;
-		return;
-	}
-
-	switch( Msg->m_Code )
-	{
-	case Comm::MSG_OK:
-	{
-		std::cout << "Comm OK" << std::endl;
-	} break;
-	case Comm::MSG_UPDATE:
-	{
-		if( Msg->m_Type == Comm::TYPE_COBJ )
-		{
-			OOWM::Mem::CObj Incoming;
-			DeserializeObj( Msg->GetData(), Incoming );
-			OOWM::SResult   Result;
-			OOWM::Mem::CObj Helper;
-			mLocalScene.getById( Helper, Incoming.getId(), &Result );
-			mLocalScene.put( Incoming );
-			if( Result.m_nCode != R_OK )
-				HandleObjectCreated( Incoming );
-			else
-				HandleObjectChanged( Incoming );
-		}
-		if( Msg->m_Type == Comm::TYPE_CSCENE )
-		{
-			DeserializeScene( Msg->GetData(), mLocalScene );
-			HandleSceneChanged();
-		}
-	} break;
-	case Comm::MSG_DEL:
-	{
-		if( Msg->m_Type == Comm::TYPE_COBJ )
-		{
-			OOWM::Mem::CObj T;
-			mLocalScene.getById( T, Msg->GetData(), 0 );
-			mLocalScene.removeById( Msg->GetData(), 0 );
-			HandleObjectDeleted( T );
-		}
-		if( Msg->m_Type == Comm::TYPE_CSCENE )
-		{
-			mLocalScene.close();
-			HandleSceneDeleted();
-		}
-	} break;
-	case Comm::MSG_ERROR:
-	{
-		HandleError( Msg->GetData() );
-	} break;
-	default:
-		break;
-	}
-
-
-	boost::shared_ptr<Comm::Message> Inc( new Comm::Message );
-	mConnection->AsyncRead( Inc,
-		boost::bind( &SharomeInterface::HandleRead, this,
-			boost::asio::placeholders::error, Inc ) );
+	if( LegacyClient )
+		LegacyClient->Poll();
 }
 
 void SharomeInterface::HandleObjectCreated( const OOWM::Mem::CObj& Obj )
@@ -291,13 +102,9 @@ void SharomeInterface::HandleObjectChanged( const OOWM::Mem::CObj& Obj )
 	Entity* OldEnt = mEntityPool.GetEntityByAttribute( "SharomeId", Obj.getId() );
 	if( OldEnt )
 	{
-		if( OldEnt->GetName() != Obj.getName() )
-			mEntityPool.RemoveEntitiesByAttributeValue<std::string>( "SharomeId", Obj.getId() );
-		else
-			SyncAttribs( OldEnt, Obj );
+		mEntityPool.RemoveEntitiesByAttributeValue<std::string>( "SharomeId", Obj.getId() );
 	}
-	else
-		CreateEntityFromCObj( mEntityPool, Obj );
+	CreateEntityFromCObj( mEntityPool, Obj );
 }
 
 void SharomeInterface::HandleObjectDeleted( const OOWM::Mem::CObj& Obj )
@@ -306,20 +113,20 @@ void SharomeInterface::HandleObjectDeleted( const OOWM::Mem::CObj& Obj )
 	                                                         Obj.getId() );
 }
 
-void SharomeInterface::HandleSceneChanged()
+void SharomeInterface::HandleSceneChanged( const OOWM::Mem::CScene& Scene )
 {
 	// remove all Sharome entities
 	mEntityPool.RemoveEntitiesByAttribute( "SharomeId" );
 
 	// add all objects of changed scene
 	std::set<std::string> Ids;
-	mLocalScene.getIdsByName( Ids, "*" );
+	Scene.getIdsByName( Ids, "*" );
 	for( std::set<std::string>::iterator i = Ids.begin();
 	     i != Ids.end();
 	     i++ )
 	{
 		OOWM::Mem::CObj O;
-		mLocalScene.getById( O, *i );
+		Scene.getById( O, *i );
 
 		CreateEntityFromCObj( mEntityPool, O );
 	}
--- SharomeInterface/SharomeInterface.h
+++ SharomeInterface/SharomeInterface.h
@@ -3,6 +3,7 @@
 
 #include <NetworkInterface.h>
 #include <Mem/Scene.h>
+#include <LegacyClientLib/AsyncClient.hpp>
 
 #include "Message.h"
 #include "Connection.h"
@@ -27,28 +28,17 @@ public:
 	bool Start();
 	bool Stop();
 
-	void HandleConnect(
-	        const boost::system::error_code& Error,
-	        boost::asio::ip::tcp::resolver::iterator
-	        EndpointIterator );
-	void HandleRead( const boost::system::error_code& Error,
-	                 boost::shared_ptr<Comm::Message> Msg );
-	void HandleWrite( const boost::system::error_code& Error );
-
 	void Poll();
 
-private:
 	void HandleObjectCreated( const OOWM::Mem::CObj& Obj );
 	void HandleObjectChanged( const OOWM::Mem::CObj& Obj );
 	void HandleObjectDeleted( const OOWM::Mem::CObj& Obj );
-	void HandleSceneChanged();
+	void HandleSceneChanged( const OOWM::Mem::CScene& Scene );
 	void HandleSceneDeleted();
 	void HandleError( std::string Msg );
 
-	OOWM::Mem::CScene mLocalScene;
-	ConnectionPtr     mConnection;
-
-	boost::asio::io_service mIOService;
+private:
+	boost::scoped_ptr<CAsyncClient> LegacyClient;
 };
 
 #endif // LEGACY_INTERFACE_H
